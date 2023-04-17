e9eda5ef319c49bf87a63278ace89c4c9afde540
Chrispy <christiancarteno@hotmail.com>
2023-04-16 17:19:20 -0700

    template I am working on, using Next js and sanity as headless cms

---
+++ sanity.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from 'sanity';
+import {deskTool} from "sanity/desk";
+import schemas from "./sanity/schemas";
+
+const config = defineConfig({
+    projectId: "ogsv16ij",
+    dataset: "production",
+    title: "My personal site",
+    apiVersion: "2023-04-16",
+    basePath: "/admin",
+    plugins: [deskTool()],
+    schema : { types : schemas}
+});
+
+export default config;
---
+++ sanity/sanity-utils.tsx
@@ -0,0 +1,43 @@
+import { createClient, groq } from "next-sanity";
+import { Project } from "@/types/Project";
+
+export async function getProjects(): Promise<Project[]> {
+  const client = createClient({
+    projectId: "ogsv16ij",
+    dataset: "production",
+    apiVersion: "2023-04-16",
+  });
+  
+return client.fetch(
+      groq`*[_type == "project"]{
+          _id,
+          _createdAt,
+          name,
+          "slug": slug.current,
+          "image": image.asset->url,
+          url,
+          content,
+      }`
+  )
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: "ogsv16ij",
+    dataset: "production",
+    apiVersion: "2023-04-16",
+  });
+    
+return client.fetch(
+  groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content,
+  }`
+  , { slug }
+);
+}
