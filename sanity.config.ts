c72324ea24baefa001dccc07f2ca65c2f23eabb7
Chrispy <christiancarteno@hotmail.com>
2023-04-20 20:32:09 -0700

    added vercel sanity deploy plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity';
 import {deskTool} from "sanity/desk";
 import schemas from "./sanity/schemas";
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 
 const config = defineConfig({
     projectId: "ogsv16ij",
@@ -8,7 +9,7 @@ const config = defineConfig({
     title: "My personal site",
     apiVersion: "2023-04-16",
     basePath: "/admin",
-    plugins: [deskTool()],
+    plugins: [deskTool(), vercelDeployTool()],
     schema : { types : schemas}
 });
 
