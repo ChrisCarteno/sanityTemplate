d3f7488036bc20d860db84049b13f16c9beb3a77
Chrispy <christiancarteno@hotmail.com>
2023-05-04 01:06:49 -0700

    added properties pages

--- app/(site)/properties/[property]/page.tsx
+++ app/(site)/properties/[property]/page.tsx
@@ -1,30 +1,30 @@
-import {getProject} from '@/sanity/sanity-utils';
+import { getProperty } from '@/sanity/sanity-utils';
 import Image from 'next/image';
 import { PortableText } from '@portabletext/react';
 
 type Props = {
-    params: {project: string};
+    params: {property: string};
 };
 
-export default async function Project({params}: Props) {
-    const slug = params.project;
-    const project = await getProject(slug);
+export default async function Property({params}: Props) {
+    const slug = params.property;
+    const property = await getProperty(slug);
 
     return (
         <div>
             <h1 className='text-5xl drop-shadow font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
-                {project.name}
+                {property.name}
             </h1>
-            <a href={project.url} title='view project' target='blank' rel='noopener noreferrer'
+            <a href={property.url} title='view property' target='blank' rel='noopener noreferrer'
             className='bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition'>
-                View Project
+                View property
             </a>
         <div className="text-lg text-gray-700 mt-5">
-            <PortableText value={project.content} />
+            <PortableText value={property.content} />
         </div>
 
-        <Image src={project.image}
-        alt={project.name} width={1200} height={800}
+        <Image src={property.image}
+        alt={property.name} width={1200} height={800}
         className='mt-10 border-2 border-gray-700 object-cover rounded-xl' />
         </div>
     )
