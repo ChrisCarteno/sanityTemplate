a986643e59c5f4f086992f128668c64915f78e72
Chrispy <christiancarteno@hotmail.com>
2023-05-06 01:15:23 -0700

    added a footer to the layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -42,6 +42,19 @@ export default async function RootLayout({
         <main className='py-10'>
           {children}
         </main>
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+          <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; 2023 <a href="https://flowbite.com/" className="hover:underline">Christian&trade;</a>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+              <li>
+                  <a href="/properties" className="mr-4 hover:underline md:mr-6 ">Properties</a>
+              </li>
+              <li>
+                  <a href="http://www.christiancarteno.com/#contact" className="hover:underline">Contact</a>
+              </li>
+          </ul>
+          </div>
+        </footer>
       </body>
     </html>
   )
