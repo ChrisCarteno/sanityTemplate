54d38ccfd78dc989d5384383be69858f61ce0a8a
Chrispy <christiancarteno@hotmail.com>
2023-05-17 22:50:11 -0700

    contact form basically finished

--- app/api/sendGrid/route.tsx
+++ app/api/sendGrid/route.tsx
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+const mail = require('@sendgrid/mail');
 
 type Feedback = {
     fullname?: string;
@@ -7,12 +8,29 @@ type Feedback = {
     message?: string;
 }
 
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
 export async function POST(request: Request){
     const data: Feedback = await request.json();
-    console.log('data: ', data)
     
     const { fullname, email, subject, message } = data;
 
+    const messageBody = `
+        Full Name: ${fullname}\r\n
+        Email: ${email}\r\n
+        Subject: ${subject}\r\n
+        Message: ${message}
+    `;
+
+    const msg = {
+        to: 'pro@christiancarteno.com', // Change to your recipient
+        from: 'hello@weaversdev.com', // Change to your verified sender
+        subject: data.subject,
+        text: messageBody,
+        html: messageBody.replace(/\r\n/g, '<br>'),
+      }
+
+    await mail.send(msg);
     return NextResponse.json({ fullname, email, subject, message})
 }
 
