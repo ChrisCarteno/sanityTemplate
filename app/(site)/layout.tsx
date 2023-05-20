21e456366dcfa039fe5369cdc94d299876752af8
Chrispy <christiancarteno@hotmail.com>
2023-05-19 21:23:19 -0700

    added to the nav

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -28,8 +28,8 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={'max-w-3xl mx-auto pt-20 pb-5 flex flex-col min-h-screen ' + nunito.className}>
-      <header className='flex items-center justify-between'>
-        <Link href="/"
+      <header>
+        {/* <Link href="/"
         className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-house" viewBox="0 0 16 16">
             <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
@@ -42,7 +42,27 @@ export default async function RootLayout({
           <Link href={'/contact'} className='hover:underline'>
               Contact
           </Link>
+        </div> */}
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+              <Link href="/"
+              className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-house" viewBox="0 0 16 16">
+                  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+              </svg>
+              </Link>
+              <div className='flex items-center gap-3'>
+          <Link href={'/properties'} className='hover:underline'>
+              Properties
+          </Link>
+          <Link href={'/contact'} className='hover:underline'>
+              Contact
+          </Link>
+          <a href="tel:17604429714" className="mr-6 text-sm  text-gray-500 dark:text-white hover:underline">(760) 442-9714</a>
         </div>
+          </div>
+        </nav>
+
         </header>
 
         <main className='py-10'>
